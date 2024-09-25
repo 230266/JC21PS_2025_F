a243ed862ee0e8e05acf0685cdc8230bfe66f82f
PDC\t-kumagai <t-kumagai@pdc.proto-g.co.jp>
2024-09-25 09:52:16 +0900

    最終微調整分（熊谷）

--- src/main/java/jp/co/jc21ps/activity_management/controller/LogoutController.java
+++ src/main/java/jp/co/jc21ps/activity_management/controller/LogoutController.java
@@ -2,19 +2,23 @@ package jp.co.jc21ps.activity_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpSession;
+import jp.co.jc21ps.activity_management.form.LoginForm;
 
 @Controller
 @RequestMapping("/logout")
 public class LogoutController {
 
     @GetMapping
-    public String logout(HttpSession session) {
-
+    public ModelAndView logout(HttpSession session) {
+        ModelAndView mav = new ModelAndView();
         // ログアウトボタン押下時、ログイン画面に遷移する
         session.invalidate();
-        return "redirect:/login";
+        mav.addObject("loginForm", new LoginForm());
+        mav.setViewName("login");
+        return mav;
     }
 
 }
